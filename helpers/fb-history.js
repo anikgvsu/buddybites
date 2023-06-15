c430070b5de7fee817359e13a5869b52611e93ca
azizHakim <azizulraihan19@gmail.com>
2023-06-15 16:47:11 -0400

    credentialsfix

--- helpers/fb-history.js
+++ helpers/fb-history.js
@@ -7,20 +7,9 @@ import {
   set,
 } from "firebase/database";
 
-//import { firebaseConfig } from "./fb-credentials";
+import { firebaseConfig } from "./fb-credentials";
 import { initializeApp } from "firebase/app";
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBF42lvXkuzJKAx8V7sKIZp9s1vgQyLO7A",
-  authDomain: "calculator-d35ba.firebaseapp.com",
-  databaseURL: "https://calculator-d35ba-default-rtdb.firebaseio.com",
-  projectId: "calculator-d35ba",
-  storageBucket: "calculator-d35ba.appspot.com",
-  messagingSenderId: "606009473062",
-  appId: "1:606009473062:web:4fc3fe0875acf6755d8ee7",
-  measurementId: "G-F876E37CEN"
-};
 
 export function initHistoryDB() {
   initializeApp(firebaseConfig);
