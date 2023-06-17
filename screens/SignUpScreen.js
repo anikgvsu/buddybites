9ee18d6fefaa2ccab719484015cf330873b2f01f
Mohammad Shafiqul Islam <islammo@mail.gvsu.edu>
2023-06-17 19:01:39 -0400

    allergy not required during sign up

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -117,7 +117,6 @@ const SignUpScreen = ({ navigation }) => {
       !usernameError &&
       !passwordError &&
       !emailError &&
-      !allergyError &&
       !favoriteFoodsError &&
       !dietHabitError
     ) {
