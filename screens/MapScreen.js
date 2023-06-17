373e16ab0481619272a5a4a8a342f4a91d533450
Mohammad Shafiqul Islam <islammo@mail.gvsu.edu>
2023-06-17 12:34:47 -0400

    interactive map in separate screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,40 +1,101 @@
-import MapView from 'react-native-maps';
+import MapView, {Marker, Circle, Callout} from 'react-native-maps';
 import axios from 'axios';
 import { maps_KEY } from "../api/api_key.js";
 import { Text, StyleSheet, View, TextInput, SafeAreaView } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Geocoder from 'react-native-geocoding';
 
 const AddressSearchBar = () => {
+  const [ pin, setPin ] = useState({
+		latitude: 42.97218609999999,
+		longitude: -85.95402179999999,
+		address: ""
+	})
 
+	console.log('current address');
+	console.log(pin);
 
-  // return (
-  //   <GooglePlacesAutocomplete
-  //     placeholder='Search'
-  //     onPress={(data, details = null) => {
-  //       // 'details' is provided when fetchDetails = true
-  //       console.log(data, details);
-  //     }}
-  //     query={{
-  //       key: maps_KEY,
-  //       language: 'en',
-  //     }}
-  //   />
-  // );
-  
-  // return (
-  //   <View style={styles.container}>
-  //     <MapView style={styles.map}
-  //     initialRegion={{
-  //       latitude: 37.78825,
-  //       longitude: -122.4324,
-  //       latitudeDelta: 0.0922,
-  //       longitudeDelta: 0.0421,
-  //     }}
-  //     provider="google" 
-  //     />
-  //   </View>
-  // );
+	const [ region, setRegion ] = useState({
+		latitude: 42.97218609999999,
+		longitude: -85.95402179999999,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421
+	})
+	useEffect(() => {
+		Geocoder.init(maps_KEY); // Initialize Geocoder with your API key
+	  }, [pin]);
+	
+	  const getPinAddress = async (latitude, longitude) => {
+		try {
+		  const response = await Geocoder.from(latitude, longitude);
+		  const address = response.results[0].formatted_address;
+		  setPin({latitude: latitude, longitude: longitude,address: address });
+		} catch (error) {
+		  console.error(error);
+		}
+	  };
+	  const handleSearchText = () => {
+		return pin.address !== '' ? pin.address : 'Search'; // Use pin address if available, otherwise use 'Search'
+	  };
+	return (
+		<View style={{ flex: 1 }}>
+			<GooglePlacesAutocomplete
+				placeholder={handleSearchText()}
+				fetchDetails={true}
+				
+				onPress={(data, details = null) => {
+					// 'details' is provided when fetchDetails = true
+					//console.log("here", data,"there", details.description)
+					setRegion({
+						latitude: details.geometry.location.lat,
+						longitude: details.geometry.location.lng,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421
+					})
+					setPin({
+						latitude: details.geometry.location.lat,
+						longitude: details.geometry.location.lng,
+						address: data.description
+         			 })
+				}}
+				query={{
+					key: maps_KEY,
+					language: "en",
+					components: "country:us",
+					location: `${region.latitude}, ${region.longitude}`
+				}}
+				styles={{
+					container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+					listView: { backgroundColor: "white" }
+				}}
+			/>
+			<MapView
+				style={styles.map}
+          		region={region}
+				provider="google"
+			>
+				
+				<Marker
+					coordinate={pin}
+					draggable={true}
+					onDragStart={(e) => {
+						//console.log("Drag start", e.nativeEvent.coordinates)
+					}}
+					onDragEnd={(e) => {
+						getPinAddress(
+							e.nativeEvent.coordinate.latitude,
+							e.nativeEvent.coordinate.longitude)
+						console.log("saved address", pin)
+					}}
+				>
+					<Callout>
+						<Text>I'm here</Text>
+					</Callout>
+				</Marker>
+			</MapView>
+		</View>
+  );
 };
 
 export default AddressSearchBar;
